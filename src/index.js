fdf49763c2f113c8e1c19fbebfc12151e51fa756
Marc Ferguson <marc@fergytech.com>
2017-10-29 17:43:28 -0400

    Added search functionality and styling.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
 const API_KEY = "AIzaSyAIdHnt2nq8ayq4RYKB_VPi4LCqoow6_Hs";
 
-const App = () => {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('witcher 3');
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
     return (
         <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+              onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+              videos={this.state.videos}
+            />
         </div>
     );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
